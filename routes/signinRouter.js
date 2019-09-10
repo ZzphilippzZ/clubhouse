48b9af82deb29ff3281f67de41dea6fa064fef9e
ZzphilippzZ <pja21393@gmail.com>
2019-09-10 15:59:35 -0600

    Add route for user_signin_post

--- routes/signinRouter.js
+++ routes/signinRouter.js
@@ -6,6 +6,6 @@ const userController = require('../controllers/user');
 /* GET home page. */
 router.get('/', userController.user_signin_get);
 
-//router.post('/', userController.user_signin_post);
+router.post('/', userController.user_signin_post);
 
 module.exports = router;
