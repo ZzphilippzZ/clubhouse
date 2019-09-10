b72dacfd9cdeb32055850030a0a063cc70de0656
ZzphilippzZ <pja21393@gmail.com>
2019-09-10 15:06:39 -0600

    Implement authenticate controller using bcrypt

--- controllers/authenticateController.js
+++ controllers/authenticateController.js
@@ -1,24 +1,28 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 
+const User = require('../models/user');
+
 passport.use(
-  new LocalStrategy((username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) return done(err);
-      if (!user) {
-        return done(null, false, { msg: "Incorrect username" });
-      }
-	bcrypt.compare(password, user.password, (err, res) => {
-	  if (res) {
-	    return done(null, user)
-	  } 
-	  else {
-	    return done(null, false, {msg: "Incorrect password"})
-	  }
-	})
-    });
-  })
+	new LocalStrategy(async (username, password, done) => {
+		let user = await getUserByUsername(username);
+		
+		if(user && await isCorrectPassword(password, user.password)) {
+			return done(null, user);
+		}
+        	return done(null, false, { msg: "Invalid credentials" });
+	
+    	})
 );
+
+async function getUserByUsername(username) {
+	return await User.findOne({username: username});
+}
+
+async function passwordsMatch(inputPassword, actualPassword) {
+	let correctPassword = await bcrypt.compare(inputPassword, actualPassword);
+	console.log(correctPassword);
+}
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
