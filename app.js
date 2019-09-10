be1c869ffa6407a8f910e0f8afa8a36db64e63e2
ZzphilippzZ <pja21393@gmail.com>
2019-09-10 13:43:52 -0600

    Fix typo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const signinRouter = require('./routes/signin');
+const signinRouter = require('./routes/signinRouter');
 
 var app = express();
 
@@ -30,7 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/signin', siginRouter);
+app.use('/signin', signinRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
