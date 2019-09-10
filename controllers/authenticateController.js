c74bc8e8197374bf1b87de1ca16e57cd2c4b4231
ZzphilippzZ <pja21393@gmail.com>
2019-09-10 14:45:17 -0600

    Change double quotes to single for uniformity

--- controllers/authenticateController.js
+++ controllers/authenticateController.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-const LocalStrategy = require("passport-local").Strategy;
+const LocalStrategy = require('passport-local').Strategy;
 
 passport.use(
   new LocalStrategy((username, password, done) => {
