cdc546a690de7287f6a3433ab174a7ab297474d3
ZzphilippzZ <pja21393@gmail.com>
2019-09-11 11:22:13 -0600

    Add redirect if no user

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,9 @@
 const Post = require('../models/post');
 
 exports.post_list_get = (req, res) => {
+	if(!req.user) {
+		res.redirect('/sign-in');
+	}
 	res.locals.currentUser = req.user;
 	res.render('post_list');
 };
