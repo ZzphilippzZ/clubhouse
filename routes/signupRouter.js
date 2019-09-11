d20279aea2baaa234267c009d40631ba604c78d1
ZzphilippzZ <pja21393@gmail.com>
2019-09-11 07:54:01 -0600

    Create separate file for signupRouter

---
+++ routes/signupRouter.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+const userController = require('../controllers/user');
+
+/* GET home page. */
+router.get('/', userController.user_create_get);
+
+router.post('/', userController.user_create_post);
+
+module.exports = router;
