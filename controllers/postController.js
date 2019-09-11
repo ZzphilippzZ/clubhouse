0ed7b580cec8cfd4ed326432c1f5a3f0ea7e5286
ZzphilippzZ <pja21393@gmail.com>
2019-09-11 14:48:37 -0600

    Implement post_create_post controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,3 +1,5 @@
+const validator = require('express-validator');
+const validatorBody = validator.body;
 const Post = require('../models/post');
 
 exports.post_list_get = async (req, res) => {
@@ -11,7 +13,9 @@ exports.post_list_get = async (req, res) => {
 };
 
 async function getPosts() {
-	return await Post.find().exec();
+	return await Post.find()
+		.populate('user')
+		.exec();
 }
 
 exports.post_create_get = (req, res) => {
@@ -21,4 +25,33 @@ exports.post_create_get = (req, res) => {
 
 	res.locals.currentUser = req.user;
 	res.render('post_form');
+};
+
+exports.post_create_post = [ 
+
+	validatorBody('title', 'Title is required').trim().isLength({min: 1}).escape(),
+	validatorBody('body', 'Body is required').trim().isLength({min: 1}).escape(),
+	
+	async (req, res, next) => {
+
+		const errors = validator.validationResult(req);
+		if(!errors.isEmpty()) {
+			res.render('post_form', {errors: errors.array()});
+		}
+		else {
+			let post = await createNewPost(req);
+			post.save(err => {
+				if(err) return next(err);
+				res.redirect('/post')
+			});
+		}
+	}
+];
+
+async function createNewPost(req) {
+	return new Post({
+		title: req.body.title,
+		body: req.body.body,
+		user: req.user._id
+	});
 }
