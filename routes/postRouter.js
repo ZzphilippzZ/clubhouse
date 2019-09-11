5b31bc41887fb32e11a3f94c176edf8faa74181d
ZzphilippzZ <pja21393@gmail.com>
2019-09-11 12:40:26 -0600

    Add post/create route

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -5,4 +5,6 @@ const postController = require('../controllers/postController');
 
 router.get('/', postController.post_list_get);
 
+router.get('/post/create', postController.post_create_get);
+
 module.exports = router;
