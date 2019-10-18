18bef088ea51603fe23771fb0249d6f5cd80c5da
ZzphilippzZ <pja21393@gmail.com>
2019-10-18 14:54:21 -0600

    Fix to follow eslint rules

--- models/post.js
+++ models/post.js
@@ -1,35 +1,33 @@
 const moment = require('moment');
 const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
+const { Schema } = mongoose.Schema;
+const { ObjectId } = Schema.Types.ObjectId;
 
 const Post = new Schema({
-	user: {
-		type: ObjectId,
-		ref: 'User',
-		required: true
-	},
-	title: {
-		type: String,
-		maxlength: 25
-	},
-	body: {
-		type: String,
-		minlength: 1,
-		trim: true,
-		required: true
-	},
-	timestamp: {
-		type: Date,
-		default: Date.now
-	}
+  user: {
+    type: ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  title: {
+    type: String,
+    maxlength: 25,
+  },
+  body: {
+    type: String,
+    minlength: 1,
+    trim: true,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 Post
-.virtual('timeAgo')
-.get(function() {
-	return moment(this.timestamp).fromNow();
-});
+  .virtual('timeAgo')
+  .get(() => moment(this.timestamp).fromNow());
 
 module.exports = mongoose.model('Post', Post);
