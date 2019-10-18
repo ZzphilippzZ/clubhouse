9112c778d729a37ca77e811fd3312895950e4c3f
ZzphilippzZ <pja21393@gmail.com>
2019-10-18 14:56:33 -0600

    Fix to follow eslint rules

--- models/user.js
+++ models/user.js
@@ -1,46 +1,46 @@
 const mongoose = require('mongoose');
 require('mongoose-type-email');
 
-const Schema = mongoose.Schema;
+const { Schema } = mongoose.Schema;
 
 const User = new Schema({
-	firstName: {
-		type: String,
-		minlength: 1,
-		maxlength: 50,
-		required: true,
-		trim: true
-	},
-	lastName: {
-		type: String,
-		minlength: 1,
-		maxlength: 50,
-		trim: true,
-		required: true
-	},
-	username: {
-		type: String,
-		minlength: 1,
-		maxlength: 20,
-		trim: true,
-		required: true
-	},
-	email: {
-		type: mongoose.SchemaTypes.Email,
-		required: true
-	},
-	password: {
-		type: String,
-		required: true
-	},
-	isMember: {
-		type: Boolean,
-		default: false
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false
-	}
+  firstName: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+    trim: true,
+    required: true,
+  },
+  username: {
+    type: String,
+    minlength: 1,
+    maxlength: 20,
+    trim: true,
+    required: true,
+  },
+  email: {
+    type: mongoose.SchemaTypes.Email,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isMember: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model('User', User);
